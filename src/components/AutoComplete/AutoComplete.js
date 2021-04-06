eb2f16929fecd753fb507c2c998bf77de3ce795e
ztoms <tommyzhao111@gmail.com>
2021-04-05 23:31:19 -0400

    Update state of App using AutoComplete from parkId change from MapComponent,  search-on-select
    (Very hacky way of doing things but it kind of works for now)

--- src/components/AutoComplete/AutoComplete.js
+++ src/components/AutoComplete/AutoComplete.js
@@ -16,10 +16,17 @@ class AutoComplete extends React.Component {
         super(props);
         // initialize Fuse database with Park POI data
         this.database = new Fuse(require("../../data/park_pois.json"), {
-            keys: ["name_location"],
+            keys: ['name_location'],
             shouldSort: true,
-            minMatchCharLength: 1
+            minMatchCharLength: 2
         })
+        this.database_ids = new Fuse(require("../../data/park_pois.json"), {
+            keys: ['safegraph_place_id'],
+            shouldSort: false,
+            minMatchCharLength: 10,
+            threshold: 0.0
+        })
+        this.onSuggestionSelected = this.onSuggestionSelected.bind(this)
       }
 
     renderSuggestion = suggestion => {
@@ -39,13 +46,18 @@ class AutoComplete extends React.Component {
          // when Enter button is pressed
         if (e.charCode === 13 || e.keyCode === 13) {
             // return the Park POI object
+            console.log("searching")
             let park_info = this.database.search(this.state.value)[0].item
+            console.log(park_info)
             this.props.setSearch({ selectedPark: park_info })
         }
     }
 
     // function to get suggestions from park database
     onSuggestionsFetchRequested = ({ value }) => {
+        if (value.length < 5){
+            return
+        }
         let results = []
         let data = this.database.search(value).slice(0, 6)
         for (let i = 0; i < data.length; i++) {
@@ -57,6 +69,22 @@ class AutoComplete extends React.Component {
     onSuggestionsClearRequested = () => {
         this.setState({ suggestions: [] })
     }
+
+    onSuggestionSelected(event, { suggestionValue }) {
+        let park_info = this.database.search(suggestionValue)[0].item
+        this.props.setSearch({ selectedPark: park_info })
+      }
+
+    componentDidUpdate(prevProps) {
+        
+        if (this.props === prevProps || !this.props.selectedParkId || (this.props.selectedParkId && this.props.selectedParkId === prevProps.selectedParkId)) {
+            return //do nothing if props didn't change
+        } else {
+            let park_info = this.database_ids.search(this.props.selectedParkId)[0].item
+            console.log(park_info)
+            this.props.setSearch({ selectedPark: park_info })
+        }
+      }
 
     render() {
         const { value, suggestions } = this.state
@@ -75,6 +103,7 @@ class AutoComplete extends React.Component {
                     suggestions={suggestions}
                     onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
                     onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+                    onSuggestionSelected={this.onSuggestionSelected}
                     getSuggestionValue={suggestion => suggestion}
                     renderSuggestion={this.renderSuggestion}
                     inputProps={inputProps}
