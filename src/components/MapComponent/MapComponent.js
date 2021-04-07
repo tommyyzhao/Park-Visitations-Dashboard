39add7d6a04cf21ed1da324ac60a82228d5b2c2a
carol37 <jinhan.li37@gmail.com>
2021-04-06 20:08:28 -0700

    Added county layer and its linked functionalities

--- src/components/MapComponent/MapComponent.js
+++ src/components/MapComponent/MapComponent.js
@@ -44,10 +44,16 @@ class MapComponent extends React.PureComponent {
       });
 
     map.on("load", () => {
+
       map.addSource('all_parks', {
         type: 'geojson',
         data: 'https://raw.githubusercontent.com/ztoms/Park-Visitations-Dashboard/main/src/data/labeled_visitor_change.geojson'
-        });
+      });
+
+      map.addSource('county_parks', {
+        type: 'geojson',
+        data: 'https://raw.githubusercontent.com/ztoms/Park-Visitations-Dashboard/main/src/data/county_change.geojson'
+      });
 
       // add layer of parks with percent-change data
       map.addLayer({
@@ -60,7 +66,7 @@ class MapComponent extends React.PureComponent {
           'circle-radius': [
               'interpolate',
               ['linear'],
-              ['get', 'visitor_counts_pre2020'],
+              ['get', 'visitor_counts_post2020'],
               1,
               3,
               10,
@@ -132,7 +138,7 @@ class MapComponent extends React.PureComponent {
           'circle-radius': [
               'interpolate',
               ['linear'],
-              ['get', 'visitor_counts_pre2020'],
+              ['get', 'visitor_counts_post2020'],
               1,
               3,
               10,
@@ -204,7 +210,7 @@ class MapComponent extends React.PureComponent {
           'circle-radius': [
               'interpolate',
               ['linear'],
-              ['get', 'visitor_counts_pre2020'],
+              ['get', 'visitor_counts_post2020'],
               1,
               3,
               10,
@@ -264,12 +270,83 @@ class MapComponent extends React.PureComponent {
           'visibility': 'none'
         }
       });
+
+      // add county layer with percent-change data
+      map.addLayer({
+        id: 'County',
+        type: 'circle',
+        source: 'county_parks',
+        minzoom: 6,
+        paint: {
+          'circle-radius': [
+              'interpolate',
+              ['linear'],
+              ['get', 'visitor_counts_post2020'],
+              1,
+              3,
+              10,
+              6,
+              100,
+              12,
+              1000,
+              24,
+              10000,
+              32
+            ],
+          'circle-blur': 0.2,
+          'circle-stroke-width': 0.1,
+          'circle-stroke-color': '#000000',
+          'circle-color': [
+            'interpolate',
+            ['linear'],
+            ['get', 'percent_change'],
+            -1,
+            'rgba(225,19,19,0.8)',
+            0,
+            'rgba(218, 218, 218, 0.885)',
+            1,
+            'rgba(19,225,19,0.8)'
+            ]
+        },
+        layout: {
+          'visibility': 'none'
+        }
+      });
+
+
+      map.addLayer({
+        'id': 'County-labels',
+        'type': 'symbol',
+        'source': 'county_parks',
+        'minzoom': 4,
+        'layout': {
+          'text-field': ['get', 'county'],
+          'text-variable-anchor': ['top', 'bottom', 'left', 'right'],
+          'text-radial-offset': 0.8,
+          'text-font': ["Roboto Regular"],
+          'text-size': {
+              'stops': [
+                  [0, 0],
+                  [3, 0],
+                  [4, 0],
+                  [5, 0],
+                  [6, 0],
+                  [7, 0],
+                  [8, 0],
+                  [10, 0],
+                  [12, 12],
+              ]
+          },
+          'visibility': 'none'
+        }
+      });
     });
 
     // Create toggleboxes to filter for different types of parks with a click event
     var id = "All";
     var id2 = "National"
     var id3 = "State"
+    var id4 = "County"
 
     var link = document.createElement('a');
     link.href = '#';
@@ -283,29 +360,36 @@ class MapComponent extends React.PureComponent {
     link3.href = '#';
     link3.textContent = id3;
 
+    var link4 = document.createElement('a');
+    link4.href = '#';
+    link4.textContent = id4;
+
     link.onclick = function(e) {
         e.preventDefault();
         e.stopPropagation();
 
-        var layers = [];
+        var layers1 = [];
 
-        layers.push('All');
-        layers.push('All-labels');
+        layers1.push('All');
+        layers1.push('All-labels');
 
-        for (var i = 0; i < layers.length; i++) {
-            var visibility = map.getLayoutProperty(layers[i], 'visibility');
+        for (var i = 0; i < layers1.length; i++) {
+            var visibility = map.getLayoutProperty(layers1[i], 'visibility');
             if (visibility === 'visible') {
-                map.setLayoutProperty(layers[i], 'visibility', 'none');
+                map.setLayoutProperty(layers1[i], 'visibility', 'none');
                 link.className = '';
             } else {
                 link.className = 'active';
                 link2.className = '';
                 link3.className = '';
-                map.setLayoutProperty(layers[i], 'visibility', 'visible');
+                link4.className = '';
+                map.setLayoutProperty(layers1[i], 'visibility', 'visible');
                 map.setLayoutProperty('National', 'visibility', 'none');
                 map.setLayoutProperty('National-labels', 'visibility', 'none');
                 map.setLayoutProperty('State', 'visibility', 'none');
                 map.setLayoutProperty('State-labels', 'visibility', 'none');
+                map.setLayoutProperty('County', 'visibility', 'none');
+                map.setLayoutProperty('County-labels', 'visibility', 'none');
             }
         }
 
@@ -360,10 +444,36 @@ class MapComponent extends React.PureComponent {
         }
     }
 
+    link4.onclick = function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        var layers4 = [];
+
+        layers4.push('County');
+        layers4.push('County-labels');
+
+        for (var l = 0; l < layers4.length; l++) {
+            var visibility4 = map.getLayoutProperty(layers4[l], 'visibility');
+            console.log(visibility4);
+            if (visibility4 === 'visible') {
+                map.setLayoutProperty(layers4[l], 'visibility', 'none');
+                link4.className = '';
+            } else {
+                link4.className = 'active';
+                link.className = '';
+                map.setLayoutProperty(layers4[l], 'visibility', 'visible');
+                map.setLayoutProperty('All', 'visibility', 'none');
+                map.setLayoutProperty('All-labels', 'visibility', 'none');
+            }
+        }
+    }
+
     var layers = document.getElementById('menu');
     layers.appendChild(link);
     layers.appendChild(link2);
     layers.appendChild(link3);
+    layers.appendChild(link4);
 
     // Color legend
     var legend = document.getElementById('color-legend');
@@ -476,6 +586,35 @@ class MapComponent extends React.PureComponent {
     });
 
     map.on('mouseleave', 'State', function () {
+      map.getCanvas().style.cursor = '';
+      popup.remove();
+    });
+
+    map.on('mouseenter', 'County', function (e) {
+      map.getCanvas().style.cursor = 'pointer';
+
+      var coordinates = e.features[0].geometry.coordinates.slice();
+      var name = e.features[0].properties.county;
+      var state = e.features[0].properties.state;
+      var pre2020 = e.features[0].properties.visitor_counts_pre2020;
+      var post2020 = e.features[0].properties.visitor_counts_post2020;
+      var change = e.features[0].properties.percent_change;
+
+      // Ensure that if the map is zoomed out such that multiple copies of the feature are visible, the popup appears over the copy being pointed to.
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
+
+      pre2020 = pre2020 ? pre2020 : "no data"
+      post2020 = post2020 ? post2020 : "no data"
+      change = change ? change : "no data"
+
+      // Populate the popup and set its coordinates based on the feature found.
+      popup.setLngLat(coordinates).setHTML(`<b>${name}</b>, ${state} <p> <b>Average Monthly Visitors</b> <br> Pre-2020: ${pre2020} <br>
+                                            Post-2020: ${post2020} <br></p> <b>Percent change:</b> ${change}`).addTo(map);
+    });
+
+    map.on('mouseleave', 'County', function () {
       map.getCanvas().style.cursor = '';
       popup.remove();
     });
