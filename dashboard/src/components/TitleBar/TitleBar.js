f055ca70578700f17005073a5161518b18a119c2
ztoms <tommyzhao111@gmail.com>
2021-04-18 19:26:00 -0400

    Updating titlebar style

--- dashboard/src/components/TitleBar/TitleBar.js
+++ dashboard/src/components/TitleBar/TitleBar.js
@@ -1,13 +1,18 @@
 import React from "react"
-import psu_logo from './psu_logo.png'
-import './styles.scss';
+import Navbar from 'react-bootstrap/Navbar';
 
 function TitleBar() {
     return (
-      <div className="titleContainer">
-        <img className="psu_logo" src={psu_logo} alt="PSU logo"></img>
-        <h1 className="title">Park Visitation Dashboard Covid-19</h1>
-      </div>
+      <Navbar bg="primary">
+        <img
+              alt="PSU logo"
+              src="/psu_logo.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{'   '}
+        <h1>   Park Visitation Dashboard Covid-19</h1>
+      </Navbar>
     )
   }
   
